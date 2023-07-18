9eeb8aa02ff3fd0564a587815799d11ef4b7b4c0
Kabelo Hlaka <kabelohlaka@gmail.com>
2023-07-18 07:35:00 -0700

    created C file 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Description: Prints the alphabet in lowercase
  * Return: Always 0 on (success)
  */
-int main(void)
+void print_alphabet(void)
 {
-	int i;
+	char letter;
 
-	for (i = 97; i <= 122; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
