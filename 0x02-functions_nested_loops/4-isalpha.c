14250fabd3482ae77a657ccecb412453b5517cb7
Kabelo Hlaka <kabelohlaka@gmail.com>
2023-07-18 14:36:55 -0700

    created C script file 4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,5 +6,17 @@
  */
 int _isalpha(int c)
 {
-	return ((c >= 'z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 97) && c <= 122)
+	{
+		return (1);
+	}
+	else if ((c= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	_putchar('\n');
 }
