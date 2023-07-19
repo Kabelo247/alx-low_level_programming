690c2476bec07686e6b051baadb6649ece3ef5ba
Kabelo Hlaka <kabelohlaka@gmail.com>
2023-07-19 14:30:19 -0700

    created C script file 100-time_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,9 +3,9 @@
  * print_times_table - Prints the n times table, starting with 0
  * @n: number of the times table
  */
-void print_times_table(int n);
+void print_times_table(int n)
 {
-	int i, j, k
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
