ddd2dbdef40b602803e118ba346b159816734d96
Kabelo Hlaka <kabelohlaka@gmail.com>
2023-07-19 02:30:21 -0700

    created C script file 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The input number is an integer
+ * Return: 1 if n is greater than zero, 0 if n is zero,
+ * -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
