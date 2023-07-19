121abb24b856e3cb11a20b3a0b5bcb1e058a1161
Kabelo Hlaka <kabelohlaka@gmail.com>
2023-07-19 07:05:14 -0700

    created C script file 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Function that adds two integers and returns the result
+ * @x: number being added
+ * @y: number being added
+ * Return: Always 0 on (success)
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
